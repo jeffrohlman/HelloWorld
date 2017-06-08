bdae13343d11ca15aaadb906bbdd49e3a6ca99ad
Jeffrey Rohlman <jeffreyrohlman@jeffreys-mbp.attlocal.net>
2017-06-08 16:13:25 -0700

    Built UI, added IBActions and IBOutlet

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func helloTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearTap(_ sender: UIButton) {
+    }
+    
 }
 
